c8cc4f3a9e44c0a0e9bbef15f56742a2983c1ab7
Phishy1337 <D00444216@utahtech.edu>
2025-08-25 10:31:18 -0600

    testing change to inline suggest

--- firstfile.py
+++ firstfile.py
@@ -1,1 +1,12 @@
-print("Hello world")
+# Modularity:
+# 
+
+def rangetest():
+    for i in range(0, 70, 1):
+        if i == 69:
+            print("Nice")
+        else:
+            print(i)
+
+
+rangetest()
